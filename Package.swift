2d73e083545da0c42eb28d7d5d13f803ee9794c9
Steven Klassen <klassens@acm.org>
2020-09-15 16:13:57 -0600

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "KSSWeb", targets: ["KSSWeb"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/klassen-software-solutions/KSSCore.git", .branch("development/v5") /*from: "4.0.0"*/),
+        .package(url: "https://github.com/klassen-software-solutions/KSSCore.git", from: "5.0.0"),
     ],
     targets: [
         .target(name: "KSSNativeUI", dependencies: [.product(name: "KSSFoundation", package: "KSSCore")]),
